ba923e88cf8e794e07b5003f0284890783001942
Wanja Tolksdorf <wanja.tolksdorf@gmx.de>
2023-01-17 20:06:37 +0100

    change data cleaning code to clean the new dataset food_data.csv: drop duplicates and missing values, clean Ingredients column, create new feature Vegan

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -1,32 +1,74 @@
 import pandas as pd
 
-# read relevant rows of food data in chunks of 100000 rows at a time
-# probably irrelevant: 'categories', 'categories_tags', 'labels', 'labels_tags'
-food_data_chunk = pd.read_csv("D:\PyCharm\Techlabs_project\data\en.openfoodfacts.org.products.csv",
-                              sep='\t', encoding="utf_8", chunksize=100000,
-                              usecols=['code', 'url', 'product_name', 'ingredients_text', 'traces', 'traces_tags'])
-food_data = pd.concat(food_data_chunk)
-
+# load food_data
+food_data = pd.read_csv("D:/PyCharm/Techlabs_project/food_data.csv", encoding="utf_8")
 
 # inspect food_data
 print(food_data.head())
 print(food_data.info())
 print(food_data.shape)
 
-# try to find certain products in food_data
-barcode = "0000000001670"
-print(food_data.loc[food_data["code"] == barcode])
-product_name = "Tofu Natur"
-print(food_data.loc[food_data['product_name'] == product_name])
+# rename the first column
+food_data = food_data.rename(columns={'Unnamed: 0': 'Old Index'})
 
-# drop duplicates
+# check for duplicate rows
 duplicateRows = food_data[food_data.duplicated()]
-print(len(duplicateRows))
-food_data = food_data.drop_duplicates()
+print(len(duplicateRows))  # there are no duplicates
+
+# check for missing values
+print(food_data.isnull().sum())  # 3142 na in Product Name, 2878 in Ingredients
+null = food_data[food_data['Ingredients'].isnull()]
+print(null)
+
+# drop rows with missing values in Ingredients
+food_data = food_data.dropna(subset="Ingredients")
+print(food_data.isnull().sum())
 print(food_data.shape)
 
-# drop rows with missing values in ingredients_text
-print(food_data.isnull().sum())
-food_data = food_data.dropna(subset="ingredients_text")
-print(food_data.isnull().sum())
-print(food_data.shape)
+# clean Ingredients column
+
+# replace underscores
+food_data["Ingredients"] = food_data["Ingredients"].apply(lambda x: x.replace("_", ""))
+
+# change uppercase?
+#food_data["Ingredients"] = food_data["Ingredients"].apply(lambda x: x.lower())  # oder .title()
+
+# delete percentages?
+
+# inspect Analysis Tags column
+print(food_data["Analysis Tags"].unique())
+# different types of vegan status: en:vegan, en:non-vegan, en:vegan-status-unknown, en:maybe-vegan
+
+# create new column Vegan
+def create_vegan(txt):
+    lst = txt.split(",")
+    for i in lst:
+        if i == "en:vegan":
+            return "vegan"
+        elif i == "en:non-vegan":
+            return "non-vegan"
+        elif i == "en:vegan-status-unknown":
+            return "vegan status unknown"
+        elif i == "en:maybe-vegan":
+            return "maybe vegan"
+
+food_data["Vegan"] = food_data["Analysis Tags"].apply(create_vegan)
+
+# inspect Vegan column
+
+# check for missing values
+print(food_data["Vegan"].isnull().sum())  # 43 missing values
+null_vegan = food_data[food_data['Vegan'].isnull()]
+# reason for missing values: no information about vegan in analysis tags (only vegetarian)
+
+# check for values with unknown vegan status/maybe vegan
+vegan_unknown = food_data[(food_data["Vegan"]=="vegan status unknown")|(food_data["Vegan"]=="maybe vegan")]
+print(len(vegan_unknown))  # 19399 times the vegan status is not known
+
+"""
+interesting finding: 127 Veganer Burger: 48,5% texturiertes Erbsenprotein (Erbsenprotein, Erbsenmehl), Maisgranulat,
+Reisgranulat,Verdickungsmittel: Methylcellulose, Sonnenblumenkerne teilent - ölt, Rapsöl, Gewürze (Knoblauch, Pfeffer 
+und Zwiebeln), Aromen, Karamellpulver (Karamellzuckersirup, Maltodextrin), 2,9 % enzymatisch hydrolysiertes 
+Erbsenprotein, Speisesalz, Rote Beete, Zucker, Raucharoma.
+"""
+
