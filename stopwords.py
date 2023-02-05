4c36a5fddd5059a3a9fa3a0b69beff3f3fbd2178
Wanja Tolksdorf <wanja.tolksdorf@gmx.de>
2023-02-05 22:34:57 +0100

    add stopwords

---
+++ stopwords.py
@@ -0,0 +1,6 @@
+"""
+Stopwords:
+und, aus, mit, von, in, an, bei, veränderlichen gewichtsanteilen/massenanteilen, kann, enthalten,
+gefangen, gekocht(e), vorgekocht, homogenisiert, pasteurisiert(er), ultrahocherhitzt, iodiert/jodiert(es), hydrolisiertes, modifizierte, teilentrahmt(e), entrahmte,
+china, europa, südamerika, kontrolliert, ökologischer/ökologischem, landwirtschaft, landbau, mindestens, ungeöffnet, haltbar
+"""
